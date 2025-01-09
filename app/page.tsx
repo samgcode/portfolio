445d2110255cbf9787bcc8a6314c456f8b077f9d
samgcode <samgcode@gmail.com>
2025-01-09 16:21:21 -0700

    header
    + add a simple headerw

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import Projects from "./projects";
 
 export default function Home() {
   return (
-    <Projects />
+    <>
+      <div className="bg-black text-white text-center py-12">
+        <h1 className="mx-auto text-6xl">Hi, I'm Sam</h1>
+        <h2 className="mx-auto text-3xl max-w-[48rem] mt-4">I do a lot of projects to learn new things, and you can check out some of them here!</h2>
+      </div>
+      <Projects />
+    </>
   );
 }
