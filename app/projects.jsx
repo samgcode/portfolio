4776d96996d520e6da46bcb719ac6de2e532dbfa
samgcode <samgcode@gmail.com>
2025-01-09 11:12:57 -0700

    add more projects
    + add portfolio website
    + add robotics team
    + add christmas hat
    + add deathlink

--- app/projects.jsx
+++ app/projects.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 
 import Carousel from './carousel'
-import "./projects.css"
 
 import projects from './projects.json'
 
@@ -47,20 +46,22 @@ export default function Projects() {
             >
               <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div className='rounded-lg'>
-                  <Carousel slides={selectedProject.images} />
+                  {selectedProject.images &&
+                    <Carousel slides={selectedProject.images} />
+                  }
                 </div>
                 <div className="pt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <h1 className="text-3xl font-semibold leading-6 text-gray-900">
                     {selectedProject.name}
                   </h1>
-                  <div className="mt-2">
+                  <div className="mt-2 flex gap-2">
                     {selectedProject.link &&
                       <a className="animated text-md text-blue-500 underline hover:text-blue-300 active:text-white" href={selectedProject.link} target="_blank">
                         Website
                       </a>
                     }
                     {selectedProject.github &&
-                      <a className="pl-2 animated text-md text-blue-500 underline hover:text-blue-300 active:text-white" href={selectedProject.github} target="_blank">
+                      <a className="animated text-md text-blue-500 underline hover:text-blue-300 active:text-white" href={selectedProject.github} target="_blank">
                         Github
                         {/* <svg xmlns="http://www.w3.org/2000/svg" height="10" width="10" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" /></svg> */}
                         {/* !Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */}
